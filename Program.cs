e1d7139d3a204c6135aba1e141500fbaebe7a134
Neha121995 <jagtapneha1212@gmail.com>
2021-07-27 23:17:14 +0530

    [Neha] Added: Welcome to MSTest User Registration Practice Problem

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MSTest User Registration Practice Problem");
+        }
+    }
+}
